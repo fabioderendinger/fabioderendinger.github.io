61bea9378c827caf24da2f39e75b7f2cd313474c
Fabio Derendinger <fabioderendinger@users.noreply.github.com>
2017-11-30 17:00:33 +0100

    custom jekyll blog post first commit

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,18 @@
 $( document ).ready(function() {
+
+  $(".button-collapse").sideNav();
     
-    $('.typed').typeIt({
-        strings: ["n entrepreneur.", " mathematician.", " machine learning engineer.", " full-stack web developer."],
-        startDelete: true,
-        speed: 80,
-        startDelay: 4000,
-        deleteSpeed: 40,
-        deleteDelay: 5000,
-        breakLines: false,
-        autoStart: true,
-        loop: true,
-        loopDelay: 4000,
+  $('.typed').typeIt({
+      strings: ["n entrepreneur.", " mathematician.", " machine learning engineer.", " full-stack web developer."],
+      startDelete: true,
+      speed: 80,
+      startDelay: 4000,
+      deleteSpeed: 40,
+      deleteDelay: 5000,
+      breakLines: false,
+      autoStart: true,
+      loop: true,
+      loopDelay: 4000,
    });
 
 
